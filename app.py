d4ded36e933c74a48bd25decb8bd0af76708031f
Pedro H Decezaro <decezaropedro@gmail.com>
2026-04-07 14:04:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from utils import (
     fraud_rate_by_merchant_category,
     top_risk_transaction_count,
     transaction_fraud_trend,
+    timechart_amount_by_fraud,
     fraud_rate_trend,
     fraud_by_channel
 )
@@ -60,17 +61,18 @@ with col2:
         st.warning("Coluna 'channel' não encontrada")
         selected_channels = None
 
-# PROCESSAMENTO
-
+# =========================
+# 📊 PROCESSAMENTO
+# =========================
 df_filtered = apply_filters(df, date_range, selected_channels)
 
 total_volume, total_amount, fraud_rate = calculate_kpis(df_filtered)
 
 
 
-
-# KPIs
-
+# =========================
+# 📊 KPIs
+# =========================
 col1, col2, col3 = st.columns(3)
 
 col1.metric("🔢 Volume Total", f"{total_volume:,}")
@@ -81,51 +83,52 @@ col3.metric("🚨 Taxa de Fraude", f"{fraud_rate:.2f}%")
 
 st.divider()
 
-
-# SÉRIE TEMPORAL
-
-col_graph02, col_graph002 = st.columns(2) 
-
-with col_graph02:
-    st.subheader("📈 Tendência de Transações vs Fraudes")
-    chart = transaction_fraud_trend(df_filtered)
-    st.altair_chart(chart)    
-with col_graph002:
-    st.subheader("🚨 Taxa de Fraude (%) ao Longo do Tempo")
-    chart = fraud_rate_trend(df_filtered)
-    st.altair_chart(chart)
-
-
-# RISCO
-
-col_graph0, col_graph01 = st.columns(2)
-
-with col_graph0:
-    st.subheader("📊 Ranking de Clientes de Alto Risco")
-    chart = top_risk_barchart(df_filtered)
-    st.altair_chart(chart)
-  
-with col_graph01:
-    st.subheader("🚨 Top Clientes: Risco vs Número de Transações")
-    chart = top_risk_transaction_count(df)
-    st.altair_chart(chart)    
-
-col_graph02, col_graph002 = st.columns(2) 
-
-with col_graph02:
-    st.subheader("📈 Tendência de Transações vs Fraudes")
-    chart = transaction_fraud_trend(df_filtered)
-    st.altair_chart(chart)    
-with col_graph002:
-    st.subheader("🚨 Taxa de Fraude (%) ao Longo do Tempo")
-    chart = fraud_rate_trend(df_filtered)
-    st.altair_chart(chart)
-
-
+# =========================
 # Divisão em abas para não poluir a tela
-tab1, tab2 = st.tabs(["📊 Análise de Risco", "📋 Dados Transacionais"])
+# =========================
+tab1, tab2, tab3, tab4 = st.tabs(["📈 Análise de Tendência", "📊 Análise de Risco", "Transações e Categoria", "📋 Dados Transacionais"])
 
+
+# =========================
+# 📈 SÉRIE TEMPORAL
+# =========================
 with tab1:
+    col_graph02, col_graph002 = st.columns(2) 
+
+
+    with col_graph02:
+        st.subheader("📈 Tendência de Transações vs Fraudes")
+        chart = transaction_fraud_trend(df_filtered)
+        st.altair_chart(chart)    
+    with col_graph002:
+        st.subheader("🚨 Taxa de Fraude (%) ao Longo do Tempo")
+        chart = fraud_rate_trend(df_filtered)
+        st.altair_chart(chart)
+
+    st.subheader("📈 Volume Financeiro por Status de Fraude")
+    chart = timechart_amount_by_fraud(df_filtered)
+    st.altair_chart(chart, use_container_width=True)   
+
+    # =========================
+    # 📈 RISCO
+    # =========================
+with tab2:
+    col_graph0, col_graph01 = st.columns(2)
+
+    with col_graph0:
+        st.subheader("📊 Ranking de Clientes de Alto Risco")
+        chart = top_risk_barchart(df_filtered)
+        st.altair_chart(chart)
+      
+    with col_graph01:
+        st.subheader("🚨 Top Clientes: Risco vs Número de Transações")
+        chart = top_risk_transaction_count(df_filtered)
+        st.altair_chart(chart)    
+
+
+
+
+with tab3:
     col_graph1, col_graph2 = st.columns(2)
     with col_graph1:
         st.title("🚨 Taxa de Fraude por Tipo de Transação")
@@ -137,7 +140,7 @@ with tab1:
         st.altair_chart(chart) 
 
 
-with tab2:
+with tab4:
     st.subheader("📋 Dados")
     st.dataframe(df_filtered)
 
